f48b90a70c92bb325c5727cfd8f0ff7eb3d58916
Nikhil Yadav <yadav.nikhil13@gmail.com>
2020-06-15 10:23:17 +0530

    star pattern

---
+++ Java1/src/first1/Starpattern1.java
@@ -0,0 +1,20 @@
+package first1;
+
+public class Starpattern1 {
+
+	public static void main(String[] args) {
+		int b=0;
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				b=b+1;
+				System.out.print(""+b);
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
